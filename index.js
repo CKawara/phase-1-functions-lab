81ab6aa4cb7f459739aa1a9b49b577f5adcb2294
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-08 17:41:58 +0300

    commit tree 88b08b12b23d06cb75d7fd69e9c7d4e2b83effbf

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+let hqBlock = 42;
+let blockDist = 264;
+function distanceFromHqInBlocks(blocks){
+    if (blocks > hqBlock){
+        return blocks - hqBlock;
+    }else{
+        return hqBlock - blocks;
+    }
+}
+function distanceFromHqInFeet(blocks){
+
+    return distanceFromHqInBlocks(blocks) * blockDist;
+}
+function distanceTravelledInFeet(startBlock, endBlock){
+    if (startBlock > endBlock){
+        return(startBlock - endBlock)* blockDist
+    }else{
+        return (endBlock - startBlock) * blockDist
+    }
+}
+function calculatesFarePrice(startBlock, endBlock){
+    let distance = distanceTravelledInFeet(startBlock, endBlock)
+    if (distance <= 400){
+        return 0;
+    }else if (distance > 400 && distance <= 2000 ){
+        return (distance-400)*0.02;
+    }else if (distance>2000 && distance < 2500){
+        return 25;
+    }else{
+        return'cannot travel that far';
+    }
+}
